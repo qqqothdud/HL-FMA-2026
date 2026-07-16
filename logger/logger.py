5e463d61c3284c8ff54f86801851ab57a6fbd56c
qqqothdud <qqqothdud@gmail.com>
2026-07-16 15:07:48 +0900

    logger.py

--- logger/logger.py
+++ logger/logger.py
@@ -1,2 +1,39 @@
-def save_log():
-    print("로그 저장")
+import csv
+import os
+
+
+def save_log(
+    time,
+    sensor_data,
+    steer,
+    mission
+):
+
+    file_exists = os.path.exists("log.csv")
+
+    with open("log.csv", "a", newline="") as file:
+
+        writer = csv.writer(file)
+
+        # 파일이 처음 생성될 때만 헤더 작성
+        if not file_exists:
+            writer.writerow([
+                "time",
+                "x",
+                "y",
+                "yaw",
+                "speed",
+                "steer",
+                "mission"
+            ])
+
+        # 실제 데이터 저장
+        writer.writerow([
+            time,
+            sensor_data["x"],
+            sensor_data["y"],
+            sensor_data["yaw"],
+            sensor_data["speed"],
+            steer,
+            mission
+        ])
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from logger.logger import save_log
 
-save_log()
+sensor_data = {
+    "x": 12.3,
+    "y": 4.8,
+    "yaw": 1.57,
+    "speed": 8.5,
+    "camera": None,
+    "obstacles": []
+}
+
+for i in range(10):
+
+    sensor_data["x"] += 0.5
+    sensor_data["y"] += 0.2
+    sensor_data["speed"] += 0.1
+
+    save_log(
+        i,
+        sensor_data,
+        0.15,
+        "DRIVE"
+    )
+
+print("로그 저장 완료!")
