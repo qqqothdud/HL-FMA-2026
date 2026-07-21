aaa6a35fce41858d6b3ea8b598382e24966677a2
qqqothdud <qqqothdud@gmail.com>
2026-07-21 16:38:33 +0900

    distance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from mission.mission import mission_planning
 from controller.controller import control
 from logger.logger import save_log
+from config.config_loader import load_config
+from planning.geometry import calculate_distance
+
+config = load_config()
 
 sensor_data = {
     "x": 12.3,
@@ -11,20 +15,32 @@ sensor_data = {
     "obstacles": []
 }
 
+waypoint = {
+    "x": 20,
+    "y": 10
+}
+
 for i in range(10):
 
     sensor_data["x"] += 0.5
     sensor_data["y"] += 0.2
     sensor_data["speed"] += 0.1
 
+    distance = calculate_distance(
+    sensor_data,
+    waypoint
+    )    
+
+    print(distance)
+
     mission = mission_planning(sensor_data)
-    steer, throttle, brake = control(sensor_data, mission)
+    steer, throttle, brake = control(sensor_data, mission, config)
 
     command = {
     "steer": steer,
     "throttle": throttle,
     "brake": brake
-}
+    }
 
     save_log(
     i,
@@ -49,3 +65,5 @@ for i in range(10):
     print(command)
 
     print("=" * 40)
+
+    print(config)
